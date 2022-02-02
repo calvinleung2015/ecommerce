90995b8e896020688bd4afc15d6c710383f8266d
Calvin <wfleung@ucsd.edu>
2022-02-02 18:54:54 -0500

    categories

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import Search from '@mui/icons-material/Search';
+import ShoppingCartOutlined from "@mui/icons-material/ShoppingCartOutlined";
+import { Badge } from '@mui/material';
+
 
 const Container = styled.div`
     height: 60px;
@@ -22,15 +25,37 @@ const Language = styled.span`
     font-size: 14px;
     cursor: pointer;
 `
+
 const SearchContainer = styled.div`
     border: 1px solid lightgray;
+    display: flex;
     align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`
+
+const Input = styled.input`
+    border:none
 `
 const Center = styled.div`
     flex: 1;
+    text-align: center;
+`
+
+const Logo = styled.h1`
+    font-weight: bold;
 `
 const Right = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
 `
 
 const Navbar = () => {
@@ -38,14 +63,23 @@ const Navbar = () => {
       <Container>
           <Wrapper>
               <Left>
-                  <Language>EN</Language>
+                  <Language>中</Language>
                   <SearchContainer>
-                      input
-                      <Search/>
+                      <Input/>
+                      <Search style={{color:"gray", fontSize:16}}/>
                   </SearchContainer>
               </Left>
-              <Center>center</Center>
-              <Right>right</Right>
+              <Center><Logo>長迎傢俬文儀有限公司</Logo></Center>
+              <Right>
+                  <MenuItem>REGISTER</MenuItem>
+                  <MenuItem>SIGN IN</MenuItem>
+                  <MenuItem>
+                    <Badge badgeContent={4} color="primary">
+                        <ShoppingCartOutlined/>
+
+                    </Badge>
+                  </MenuItem>
+              </Right>
           </Wrapper>
       </Container>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 //rafce - short-cut
 import React from 'react';
+import Announcement from '../components/Announcement';
+import Categories from '../components/Categories';
 import Navbar from '../components/Navbar';
+import Slider from "../components/Slider";
+
 
 const Home = () => {
   return (
     <div>
+      <Announcement/>
       <Navbar/>
+      <Slider/>
+      <Categories/>
     </div>
   );
 };
